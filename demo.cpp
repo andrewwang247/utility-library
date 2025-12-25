e04bd7d28ee2bec98dde103149d020f118b24176
Andrew Wang <siweiw9@gmail.com>
2025-12-25 11:01:33 -0500

    helper print_container function

--- demo.cpp
+++ demo.cpp
@@ -25,6 +25,9 @@ using std::unordered_map;
 using std::unordered_set;
 using std::vector;
 
+template <typename Container>
+void print_container(const Container &container);
+
 void demo_contains();
 void demo_enumerate();
 void demo_io();
@@ -41,6 +44,14 @@ int main() {
   demo_range();
   demo_sequence();
   demo_zip();
+}
+
+template <typename Container>
+void print_container(const Container &container) {
+  for (const auto &item : container) {
+    cout << item << ' ';
+  }
+  cout << '\n';
 }
 
 void demo_contains() {
@@ -70,21 +81,13 @@ void demo_enumerate() {
   cout << "\n--- ENUMERATE DEMO ---\n";
   const vector<string> vec{"iterate", "over", "this", "with", "the", "index"};
   cout << "Original list:\n\t";
-  for (const auto &s : vec) {
-    cout << s << ' ';
-  }
-  cout << '\n';
+  print_container(vec);
 
   cout << "For-range loop, start at 7:\n\t";
-  for (auto pr : enumerate(vec, 7)) {
-    cout << pr << ' ';
-  }
+  print_container(enumerate(vec, 7));
 
-  cout << "\nRange of pairs, default start:\n\t";
-  for (auto pr : enumerate(vec)) {
-    cout << pr << ' ';
-  }
-  cout << '\n';
+  cout << "Range of pairs, default start:\n\t";
+  print_container(enumerate(vec));
 }
 
 void demo_io() {
@@ -93,16 +96,13 @@ void demo_io() {
   const char* argv[5] = {"demo", "-42", "47", "-35", "12"};
   const auto args = argparse<int>(argc, argv);
   cout << "Command line args: ";
-  for (auto arg : args) cout << arg << ' ';
-  cout << '\n';
+  print_container(args);
 
   unordered_map<string, size_t> counter{{"characters", wc<char>("io.h")},
                                         {"words", wc<string>("io.h")},
                                         {"lines", wc<line>("io.h")}};
-  cout << "Stats for io.h:\n";
-  for (const auto& pair : counter) {
-    cout << '\t' << pair << '\n';
-  }
+  cout << "Stats for io.h:\n\t";
+  print_container(counter);
 }
 
 void demo_product() {
@@ -111,38 +111,25 @@ void demo_product() {
   const string s2 = "123";
 
   cout << "Iterating over cartesian product: \"abc\" x \"123\".\n\t";
-
-  for (auto pr : product(s1, s2)) {
-    cout << pr << ' ';
-  }
-  cout << '\n';
+  print_container(product(s1, s2));
 
   cout << "Iterating over cartesian product: \"123\" x \"abc\".\n\t";
-  for (auto pr : product(s2, s1)) {
-    cout << pr << ' ';
-  }
-  cout << '\n';
+  print_container(product(s2, s1));
 }
 
 void demo_range() {
   cout << "\n--- RANGE DEMO ---\n";
   cout << "range(10): ";
-  for (auto i : range(10)) {
-    cout << i << ' ';
-  }
-  cout << "\nrange(-7): ";
-  for (auto i : range(-7)) {
-    cout << i << ' ';
-  }
-  cout << "\nrange(-5, 4): ";
-  for (auto i : range(-5, 4)) {
-    cout << i << ' ';
-  }
-  cout << "\nrange(4, -5): ";
-  for (auto i : range(4, -5)) {
-    cout << i << ' ';
-  }
-  cout << '\n';
+  print_container(range(10));
+
+  cout << "range(-7): ";
+  print_container(range(-7));
+
+  cout << "range(-5, 4): ";
+  print_container(range(-5, 4));
+
+  cout << "range(4, -5): ";
+  print_container(range(4, -5));
 }
 
 void demo_sequence() {
@@ -153,35 +140,24 @@ void demo_sequence() {
 
   const auto sl1 = slice(nums, -1, 2, -2);
   cout << "\tnums[-1:2:-2]: ";
-  for (auto i : sl1) {
-    cout << i << ' ';
-  }
-  cout << '\n';
+  print_container(sl1);
 
   const auto sl2 = slice(nums, 3, 8, 2);
   cout << "\tnums[3:8:2]: ";
-  for (auto i : sl2) {
-    cout << i << ' ';
-  }
-  cout << '\n';
+  print_container(sl2);
 
   const string wd = "watch_dogs_2";
   cout << "Original string: " << wd << '\n';
   const auto tokens = split(wd, '_');
   cout << "After splitting on underscore: ";
-  for (auto token : tokens) {
-    cout << token << ' ';
-  }
+  print_container(tokens);
+
   auto restored = join(tokens.begin(), tokens.end(), "**");
-  cout << "\nAfter rejoining with double star: " << restored << '\n';
+  cout << "After rejoining with double star: " << restored << '\n';
   const string str_wd = "&*watch&*dogs&*2&*";
   const string delim = "&*";
   cout << "Splitting " << str_wd << " on &*: ";
-  auto str_toks = split(str_wd, delim);
-  for (auto token : str_toks) {
-    cout << token << ' ';
-  }
-  cout << '\n';
+  print_container(split(str_wd, delim));
 }
 
 void demo_zip() {
@@ -196,15 +172,9 @@ void demo_zip() {
   cout << '\n';
   cout << '\t' << str << '\n';
 
-  cout << "Numbers then letters:\n";
-  for (auto pr : zip(vec, str)) {
-    cout << '\t' << pr << ' ';
-  }
+  cout << "Numbers then letters:\n\t";
+  print_container(zip(vec, str));
 
-  cout << "\nLetters then numbers:\n";
-  const auto join = zip(str, vec);
-  for (auto pr : join) {
-    cout << '\t' << pr << ' ';
-  }
-  cout << '\n';
+  cout << "Letters then numbers:\n\t";
+  print_container(zip(str, vec));
 }
